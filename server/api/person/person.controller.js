d159e4d015e208108f0d24936a4d5d78a306eb61
RTANC <wongsathon_n@rtanc.ac.th>
2019-08-01 15:34:03 +0700

    finish person license expire

--- server/api/person/person.controller.js
+++ server/api/person/person.controller.js
@@ -27,37 +27,28 @@ exports.getLicenseExpire = async (req, res, next) => {
         })
         persons.forEach(val => {
             if (val.person_licenses.length !== 0) {
-                val.person_licenses[0].expire_date = moment(val.person_licenses[0].license_date).add(5, 'y').subtract(1, 'd')
                 const pl = val.person_licenses[0]
-                const dd = moment(pl.expire_date).diff(moment(), 'months')
+                pl.expire_date = moment(pl.license_date).add(5, 'y').subtract(1, 'd')
+                let dd = moment(pl.expire_date).diff(moment(), 'months')
                 if (dd < 0) {
-                    expires.expired.push({
-                        fullname: (val.gender === 2) ? val.rank.rank_abbr + 'หญิง ' + val.person_name + ' ' + val.person_surname : val.rank.rank_abbr + ' ' + val.person_name + ' ' + val.person_surname,
-                        license_no: pl.license_no,
-                        license_date: pl.license_date,
-                        expire_date: pl.expire_date
-                    })
-                } else if (dd >= 0 && dd <= 3) {
-                    expires.three.push({
-                        fullname: (val.gender === 2) ? val.rank.rank_abbr + 'หญิง ' + val.person_name + ' ' + val.person_surname : val.rank.rank_abbr + ' ' + val.person_name + ' ' + val.person_surname,
-                        license_no: pl.license_no,
-                        license_date: pl.license_date,
-                        expire_date: pl.expire_date
-                    })
+                    const mul = (Math.floor(dd / 60) * 5) * -1
+                    pl.license_date = moment(pl.license_date).add(mul, 'y')
+                    pl.expire_date = moment(pl.license_date).add(5, 'y').subtract(1, 'd')
+                    dd = moment(pl.expire_date).diff(moment(), 'months')
+                }
+                const teacher = {
+                    fullname: (val.gender === 2) ? val.rank.rank_abbr + 'หญิง ' + val.person_name + ' ' + val.person_surname : val.rank.rank_abbr + ' ' + val.person_name + ' ' + val.person_surname,
+                    license_no: pl.license_no,
+                    license_date: moment(pl.license_date).add(543, 'y').format('DD MMMM YYYY'),
+                    expire_date: moment(pl.expire_date).add(543, 'y').format('DD MMMM YYYY'),
+                    dd: dd
+                }
+                if (dd >= 0 && dd <= 3) {
+                    expires.three.push(teacher)
                 } else if (dd > 3 && dd <= 6) {
-                    expires.six.push({
-                        fullname: (val.gender === 2) ? val.rank.rank_abbr + 'หญิง ' + val.person_name + ' ' + val.person_surname : val.rank.rank_abbr + ' ' + val.person_name + ' ' + val.person_surname,
-                        license_no: pl.license_no,
-                        license_date: pl.license_date,
-                        expire_date: pl.expire_date
-                    })
+                    expires.six.push(teacher)
                 } else if (dd > 6 && dd <= 9) {
-                    expires.nine.push({
-                        fullname: (val.gender === 2) ? val.rank.rank_abbr + 'หญิง ' + val.person_name + ' ' + val.person_surname : val.rank.rank_abbr + ' ' + val.person_name + ' ' + val.person_surname,
-                        license_no: pl.license_no,
-                        license_date: pl.license_date,
-                        expire_date: pl.expire_date
-                    })
+                    expires.nine.push(teacher)
                 }
             }
         })
@@ -72,11 +63,28 @@ exports.getRetire = async (req, res, next) => {
         let result = {
             year: [0, 0],
             fiveYears: [0, 0],
-            tenYears: [0, 0]
+            tenYears: [0, 0],
+            yearList: {
+                teachers: [],
+                supports: []
+            },
+            fiveYearList: {
+                teachers: [],
+                supports: []
+            },
+            tenYearList: {
+                teachers: [],
+                supports: []
+            }
         }
         let startDate = moment((parseInt(req.query.year) - 604) + '-10-01').format()
         let endDate = moment((parseInt(req.query.year) - 603) + '-09-30').format()
-        result.year[0] = await Person.count({
+        result.yearList.teachers = await Person.findAll({
+            attributes: ['person_name', 'person_surname', 'gender', 'person_id'],
+            include: [{
+                model: Rank,
+                attributes: ['rank_abbr']
+            }],
             where: {
                 birthday: {
                     $between: [startDate, endDate]
@@ -96,7 +104,12 @@ exports.getRetire = async (req, res, next) => {
                 }]
             }
         })
-        result.year[1] = await Person.count({
+        result.yearList.supports = await Person.findAll({
+            attributes: ['person_name', 'person_surname', 'gender', 'person_id'],
+            include: [{
+                model: Rank,
+                attributes: ['rank_abbr']
+            }],
             where: {
                 birthday: {
                     $between: [startDate, endDate]
@@ -112,9 +125,14 @@ exports.getRetire = async (req, res, next) => {
                 }]
             }
         })
-
+        startDate = moment((parseInt(req.query.year) - 603) + '-10-01').format()
         endDate = moment((parseInt(req.query.year) - 599) + '-09-30').format()
-        result.fiveYears[0] = await Person.count({
+        result.fiveYearList.teachers = await Person.findAll({
+            attributes: ['person_name', 'person_surname', 'gender', 'person_id'],
+            include: [{
+                model: Rank,
+                attributes: ['rank_abbr']
+            }],
             where: {
                 birthday: {
                     $between: [startDate, endDate]
@@ -134,7 +152,12 @@ exports.getRetire = async (req, res, next) => {
                 }]
             }
         })
-        result.fiveYears[1] = await Person.count({
+        result.fiveYearList.supports = await Person.findAll({
+            attributes: ['person_name', 'person_surname', 'gender', 'person_id'],
+            include: [{
+                model: Rank,
+                attributes: ['rank_abbr']
+            }],
             where: {
                 birthday: {
                     $between: [startDate, endDate]
@@ -150,9 +173,14 @@ exports.getRetire = async (req, res, next) => {
                 }]
             }
         })
-
+        startDate = moment((parseInt(req.query.year) - 599) + '-10-01').format()
         endDate = moment((parseInt(req.query.year) - 594) + '-09-30').format()
-        result.tenYears[0] = await Person.count({
+        result.tenYearList.teachers = await Person.findAll({
+            attributes: ['person_name', 'person_surname', 'gender', 'person_id'],
+            include: [{
+                model: Rank,
+                attributes: ['rank_abbr']
+            }],
             where: {
                 birthday: {
                     $between: [startDate, endDate]
@@ -172,7 +200,12 @@ exports.getRetire = async (req, res, next) => {
                 }]
             }
         })
-        result.tenYears[1] = await Person.count({
+        result.tenYearList.supports = await Person.findAll({
+            attributes: ['person_name', 'person_surname', 'gender', 'person_id'],
+            include: [{
+                model: Rank,
+                attributes: ['rank_abbr']
+            }],
             where: {
                 birthday: {
                     $between: [startDate, endDate]
@@ -188,6 +221,12 @@ exports.getRetire = async (req, res, next) => {
                 }]
             }
         })
+        result.year[0] = result.yearList.teachers.length
+        result.year[1] = result.yearList.supports.length
+        result.fiveYears[0] = result.fiveYearList.teachers.length
+        result.fiveYears[1] = result.fiveYearList.supports.length
+        result.tenYears[0] = result.tenYearList.teachers.length
+        result.tenYears[1] = result.tenYearList.supports.length
         res.status(200).send(result)
     } catch (error) {
         next(error)
--- server/app.js
+++ server/app.js
@@ -2,14 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
+const moment = require('moment')
 const api = require('./api')
 
 app.use(bodyParser.json())
 app.use(cors())
-
+moment.locale('th')
 app.use('/api', api)
 
-app.use((err ,req, res, next) => {
+app.use((err, req, res, next) => {
     res.status(err.status || 500).send({
         error: {
             message: err.message
